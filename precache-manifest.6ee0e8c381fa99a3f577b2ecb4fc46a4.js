69250dcb4859c912e601cb159207e2c7e8230d6e
Keet Sugathadasa <keetmalin.13@cse.mrt.ac.lk>
2021-05-07 01:12:40 +0530

    Updates

---
+++ precache-manifest.6ee0e8c381fa99a3f577b2ecb4fc46a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bc9e5270530e8e057130f6b98f933ae",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2101f53bdc5c65ed11c8",
+    "url": "./static/css/main.1b9bcb54.chunk.css"
+  },
+  {
+    "revision": "6ab015c76ae38f993a2c",
+    "url": "./static/js/2.e3ea075a.chunk.js"
+  },
+  {
+    "revision": "6472a7d1d95598048b2f7b02095da167",
+    "url": "./static/js/2.e3ea075a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2101f53bdc5c65ed11c8",
+    "url": "./static/js/main.13826709.chunk.js"
+  },
+  {
+    "revision": "f68f710af401fcd1c88f",
+    "url": "./static/js/runtime-main.d4a0bd28.js"
+  }
+]);
